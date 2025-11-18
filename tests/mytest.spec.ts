e0cf30f3064241812637388a13969a35ce8d1fe6
Francisco Duran <frandugo@ymail.com>
2025-11-18 07:05:15 -0500

    Omitting test-test

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 
 test.describe("Global test", () => {
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto('https://automationexercise.com/');
-    })
+    // test.beforeEach(async ({ page }) => {
+    //     await page.goto('https://automationexercise.com/');
+    // })
 
     // test('my test', async ({ page }) => {
     //     await expect(page.locator('#newButtonName')).toBeVisible();
@@ -14,26 +14,26 @@ test.describe("Global test", () => {
     //     await expect(page.locator('#updatingButton')).toHaveText('test');
     // });
 
-    test('testing store', async ({ page }) => {
-        await page.mouse.wheel(0,500)
-        const firstProductCard = page.locator("#cartModal + .col-sm-4 .single-products")
-		await firstProductCard.hover()
+    // test('testing store', async ({ page }) => {
+    //     await page.mouse.wheel(0,500)
+    //     const firstProductCard = page.locator("#cartModal + .col-sm-4 .single-products")
+	// 	await firstProductCard.hover()
 
-        const cardOverlay = page.locator("#cartModal + .col-sm-4 .product-overlay")
-        const cardOverlayDisplayAttribute = await cardOverlay.evaluate((e) =>
-            window.getComputedStyle((e as HTMLDivElement)).display
-        )
-        await expect(cardOverlayDisplayAttribute).toBe("block")
+    //     const cardOverlay = page.locator("#cartModal + .col-sm-4 .product-overlay")
+    //     const cardOverlayDisplayAttribute = await cardOverlay.evaluate((e) =>
+    //         window.getComputedStyle((e as HTMLDivElement)).display
+    //     )
+    //     await expect(cardOverlayDisplayAttribute).toBe("block")
 
-        await page.click("#cartModal + .col-sm-4 .overlay-content > a.add-to-cart")
+    //     await page.click("#cartModal + .col-sm-4 .overlay-content > a.add-to-cart")
 
-        const modal = page.locator("#cartModal")
-        await expect(modal).toHaveClass(/show/)
+    //     const modal = page.locator("#cartModal")
+    //     await expect(modal).toHaveClass(/show/)
 
-        await page.click("button[data-dismiss='modal']")
-await expect(modal).toHaveClass(/fade/)
+    //     await page.click("button[data-dismiss='modal']")
+    //     await expect(modal).toHaveClass(/fade/)
 
-    })
+    // })
 }) 
 
 
